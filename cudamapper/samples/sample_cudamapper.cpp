b828b142745cea4ab680934e4924ae0353a93c4e
nvvishanthi <vishanthi@nvidia.com>
2020-07-18 21:03:18 -0700

    restructure code

--- cudamapper/samples/sample_cudamapper.cpp
+++ cudamapper/samples/sample_cudamapper.cpp
@@ -9,7 +9,7 @@
 */
 
 #include <claraparabricks/genomeworks/cudamapper/cudamapper.hpp>
-#include <claraparabricks/genomeworks/io/fasta_parser.hpp> // may not need this
+#include <claraparabricks/genomeworks/io/fasta_parser.hpp>
 #include <claraparabricks/genomeworks/utils/cudautils.hpp>
 #include <claraparabricks/genomeworks/utils/signed_integer_utils.hpp>
 #include "../src/index_descriptor.hpp"
@@ -27,39 +27,15 @@
 using namespace  claraparabricks::genomeworks;
 using namespace  claraparabricks::genomeworks::cudamapper;
 
-// Finish function signature
-// Note that BatchofIndices is defined in an internal header... might not be able to use here
-// std::unique_ptr<Index> initialize_batch(/*fill in function signature here*/)
-// {
-//         // Get device information.
-//     int32_t device_count = 0;
-//     GW_CU_CHECK_ERR(cudaGetDeviceCount(&device_count));
-//     assert(device_count > 0);
+// define constants. Taken from application parser
+#define INDEX_SIZE 30
+#define KMER_SIZE  15
+#define WINDOWS_SIZE 10
 
-//     // TODO VI: I don't think I need this
-//     // size_t total = 0, free = 0;
-//     // cudaSetDevice(0); // Using first GPU for sample.
-//     // cudaMemGetInfo(&free, &total);
-
-//     Init();
-
-//     const int32_t device_id = 0;
-//     cudaStream_t stream     = 0;
-
-//     std::unique_ptr<Index> batch = /*something*/; // create the batch here
-
-//     return std::move(batch);
-// }
-
-void process_batch()
-{
-    return;
-}
-
+// prints overlaps to stdout
 void print_overlaps(const std::vector<Overlap>& overlaps,
                const io::FastaParser& query_parser,
-               const io::FastaParser& target_parser,
-               const int32_t kmer_size)
+               const io::FastaParser& target_parser)
 {
     const int64_t number_of_overlaps_to_print = get_size<int64_t>(overlaps);
 
@@ -102,7 +78,7 @@ void print_overlaps(const std::vector<Overlap>& overlaps,
                                                      target_parser.get_sequence_by_id(overlaps[i].target_read_id_).seq.length(),
                                                      overlaps[i].target_start_position_in_read_,
                                                      overlaps[i].target_end_position_in_read_,
-                                                     overlaps[i].num_residues_ * kmer_size, // Print out the number of residue matches multiplied by kmer size to get approximate number of matching bases
+                                                     overlaps[i].num_residues_ * KMER_SIZE, // Print out the number of residue matches multiplied by kmer size to get approximate number of matching bases
                                                      std::max(std::abs(static_cast<int64_t>(overlaps[i].target_start_position_in_read_) - static_cast<int64_t>(overlaps[i].target_end_position_in_read_)),
                                                               std::abs(static_cast<int64_t>(overlaps[i].query_start_position_in_read_) - static_cast<int64_t>(overlaps[i].query_end_position_in_read_))), //Approximate alignment length
                                                      255);
@@ -116,6 +92,74 @@ void print_overlaps(const std::vector<Overlap>& overlaps,
     }
 
     printf("%s", buffer.data());
+}
+
+// Finish function signature
+std::unique_ptr<Index> initialize_batch(const std::shared_ptr<const io::FastaParser> parser,
+                                        DefaultDeviceAllocator allocator)
+{
+    // Get device information.
+    int32_t device_count = 0;
+    GW_CU_CHECK_ERR(cudaGetDeviceCount(&device_count));
+    assert(device_count > 0);
+
+    //Init();
+
+    // group reads into indices. For small inputs, there will only be 1 index
+    std::vector<IndexDescriptor> index_descriptors  = group_reads_into_indices(*parser,
+                                                                               INDEX_SIZE * 1'000'000);
+    // Put all reads into a single index.
+    std::unique_ptr<Index> index = Index::create_index(allocator,
+                                                       *parser,
+                                                       index_descriptors.front().first_read(),
+                                                       index_descriptors.front().first_read() + index_descriptors.front().number_of_reads(),
+                                                       KMER_SIZE,
+                                                       WINDOWS_SIZE);
+    return std::move(index);
+}
+
+void process_batch(Index& query_index,
+                   Index& target_index,
+                   const std::shared_ptr<const io::FastaParser> query_parser,
+                   const std::shared_ptr<const io::FastaParser> target_parser,
+                   DefaultDeviceAllocator allocator,
+                   bool print)
+{
+    // find anchors & find overlaps
+    auto matcher = Matcher::create_matcher(allocator,
+                                           query_index,
+                                           target_index);
+
+    // Output vector of overlaps
+    std::vector<Overlap> overlaps;
+    OverlapperTriggered overlapper(allocator);
+
+    // Constant for overlapper
+    int32_t min_residues                    = 3;
+    int32_t min_overlap_len                 = 250;
+    int32_t min_bases_per_residue           = 1000;
+    float min_overlap_fraction              = 0.8;
+
+    overlapper.get_overlaps(overlaps,
+                            matcher->anchors(),
+                            min_residues,
+                            min_overlap_len,
+                            min_bases_per_residue,
+                            min_overlap_fraction);
+
+    // reset the matcher
+    matcher.reset(nullptr);
+
+    // post process the overlaps
+    Overlapper::post_process_overlaps(overlaps);
+
+    // print the overlaps
+    if (print)
+    {
+        print_overlaps(overlaps, *query_parser, *target_parser);
+    }
+
+    return;
 }
 
 int main(int argc, char** argv)
@@ -139,94 +183,33 @@ int main(int argc, char** argv)
     }
 
     // print help string
-    // TODO VI: fix this to reflect CUDA Mapper args
     if (help)
     {
         std::cout << "CUDA Mapper API sample program. Runs minimizer-based approximate mapping" << std::endl;
         std::cout << "Usage:" << std::endl;
-        std::cout << "./sample_cudamapper [-h]" << std::endl;
-        std::cout << "-p : Print the MSA or consensus output to stdout" << std::endl;
+        std::cout << "./sample_cudamapper [-p] [-h]" << std::endl;
+        std::cout << "-p : Print the overlaps to stdout" << std::endl;
         std::cout << "-h : Print help message" << std::endl;
         std::exit(0);
     }
 
-    // Load FASTA/FASTQ file. Hardcoded for now, do we want to allow for any file to be passed in?
+    // Load FASTA/FASTQ file. Assume all-to-all
     const std::string query_file = std::string(CUDAMAPPER_BENCHMARK_DATA_DIR) + "/20_reads.fasta";
-    const std::string target_file = std::string(CUDAMAPPER_BENCHMARK_DATA_DIR) + "/20_reads.fasta"; // What does this need to be?
+    const std::string target_file = std::string(CUDAMAPPER_BENCHMARK_DATA_DIR) + "/20_reads.fasta";
 
-    // Group reads into index
     const std::size_t max_gpu_memory = cudautils::find_largest_contiguous_device_memory_section();
     DefaultDeviceAllocator allocator = create_default_device_allocator(max_gpu_memory);
 
-
-    // abstract the batchs setup into initialize_batch()
-    // BEGIN intialize_batch
-    // std::unique_ptr<Index> batch = initialize_batch(); // fix this
-
-    // create FASTA parser here
-    uint32_t kmer_size    = 15;
-    uint32_t windows_size = 10;
+    // create FASTA parser
     std::shared_ptr<io::FastaParser> query_parser;
     std::shared_ptr<io::FastaParser> target_parser;
-    query_parser = io::create_kseq_fasta_parser(query_file, 1, false);   // defaults taken from application parser
-    // assume all-to-all
-    target_parser = query_parser;
+    query_parser = io::create_kseq_fasta_parser(query_file, KMER_SIZE + WINDOWS_SIZE -1);   // defaults taken from application parser
+    target_parser = query_parser;   // assume all to all
 
-    // group reads into indices
+    std::unique_ptr<Index> query_index  = initialize_batch(query_parser, allocator);
+    std::unique_ptr<Index> target_index = initialize_batch(target_parser, allocator);
 
-    // split indices into IndexDescriptors - these two pieces make up a single batch. This hsould be fine as.
-    std::vector<IndexDescriptor> query_index_descriptors  = group_reads_into_indices(*query_parser,
-                                                                                    30 * 1'000'000);
-    std::vector<IndexDescriptor> target_index_descriptors = group_reads_into_indices(*target_parser,
-                                                                                     30 * 1'000'000);
-
-
-    // might need to duplicate the above to for the device.. maybe not?
-
-    // END initialize_batch
-
-    std::unique_ptr<Index> query_index  = Index::create_index(allocator,
-                                                              *query_parser,
-                                                              query_index_descriptors[0].first_read(),
-                                                              query_index_descriptors[0].first_read() + query_index_descriptors[0].number_of_reads(),
-                                                              kmer_size,
-                                                              windows_size);   // leave other default arguments as-is
-    std::unique_ptr<Index> target_index = Index::create_index(allocator,
-                                                              *target_parser,
-                                                              target_index_descriptors[0].first_read(),
-                                                              target_index_descriptors[0].first_read() + target_index_descriptors[0].number_of_reads(),
-                                                              kmer_size,
-                                                              windows_size);   // leave other default arguments as-is
-
-    // // TODO VI: Abstract the following to process_batch function?
-    // // for each pair of indices, find anchors & find overlaps
-    auto matcher = Matcher::create_matcher(allocator,
-                                           *query_index,
-                                           *target_index);
-
-    //Overlapper overlapper(allocator);
-    std::vector<Overlap> overlaps;
-    OverlapperTriggered overlapper(allocator);
-
-    int32_t min_residues                    = 3;     // r, recommended range: 1 - 10. Higher: more accurate. Lower: more sensitive
-    int32_t min_overlap_len                 = 250;   // l, recommended range: 100 - 1000
-    int32_t min_bases_per_residue           = 1000;  // b
-    float min_overlap_fraction              = 0.8;   // z
-    overlapper.get_overlaps(overlaps,
-                            matcher->anchors(),
-                            min_residues,
-                            min_overlap_len,
-                            min_bases_per_residue,
-                            min_overlap_fraction);
-
-    // reset the matcher
-    matcher.reset(nullptr);
-
-    // post process the overlaps
-    Overlapper::post_process_overlaps(overlaps);
-    // print the overlaps
-    print_overlaps(overlaps, *query_parser, *target_parser, kmer_size);
-    std::cout << "reached the end" << std::endl;
+    process_batch(*query_index, *target_index, query_parser, target_parser, allocator, print);
 
     return 0;
 }
